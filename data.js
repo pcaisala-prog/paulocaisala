e86b95b5ae9477cfb3649a975cbca2f08d358c7a
pcaisala-prog <pcaisala@gmail.com>
2025-10-02 23:01:23 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+const namaSaya = "paulo caisala";
+let usia = 17;
+
+
+function generateBiodata() {
+console.log(`nama saya adalah ${namaSaya} dan usia saya ${usia} tahun`);
+}
+
+console.log(nama);
+console.log(usia);
+generateBiodata
